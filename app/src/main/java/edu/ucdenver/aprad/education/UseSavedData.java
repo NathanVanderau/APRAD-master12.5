5008ed0cd9bed42b2f6494972ac3cfa10611df6f
NathanVanderau <Nathan.Vanderau@ucdenver.edu>
2014-12-07 15:37:08 -0700

    Updated the Education section to start using the "Use Saved Data" Section. Updated the AndroidManifest.xml. Wrote the FileChooser.java which allows the user to navigate the SD Card, FileArrayAdapter.java which is our customized Array Adapter, and open_file.xml which is the UI. Debugged and Tested the above. Seems to work well.

--- app/src/main/java/edu/ucdenver/aprad/education/UseSavedData.java
+++ app/src/main/java/edu/ucdenver/aprad/education/UseSavedData.java
@@ -11,5 +11,17 @@ import android.app.Activity;
 
 public class UseSavedData extends Activity
 {
-
+    /********************************
+     **
+     **         !!TO DO!!
+     **
+     ** When we implement the ability to graph
+     ** previously captured and saved wave data,
+     ** we will do so here.
+     ** We will need to call FileChooser, open
+     ** the selected CSV file, push the data
+     ** through the FFT, and graph the returned
+     ** data.
+     **
+     ********************************/
 }
